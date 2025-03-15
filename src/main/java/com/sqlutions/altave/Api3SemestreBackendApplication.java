4d6f236cb9026b372c5cf14a50dfd292d7e3edb1
DaviSFS21 <davisfs2110@gmail.com>
2025-03-15 00:34:19 -0300

    build: configura o ambiente com spring e o inclui no docker

---
+++ src/main/java/com/sqlutions/altave/Api3SemestreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sqlutions.altave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Api3SemestreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Api3SemestreBackendApplication.class, args);
+    }
+
+}
