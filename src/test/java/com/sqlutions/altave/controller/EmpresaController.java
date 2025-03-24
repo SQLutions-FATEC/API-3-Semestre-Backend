14ae128f6d570d8f863f54b27c52df63050377cd
Glória Brito <gloriafbrito@gmail.com>
2025-03-24 17:36:10 -0300

    feat: cria endpoint POST para cadastro de empresa em EmpresaController

---
+++ src/test/java/com/sqlutions/altave/controller/EmpresaController.java
@@ -0,0 +1,26 @@
+package com.sqlutions.altave.controller;
+
+import com.sqlutions.altave.dto.EmpresaDTO;
+import com.sqlutions.altave.model.Empresa;
+import com.sqlutions.altave.service.EmpresaService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/empresa")
+public class EmpresaController {
+
+    private final EmpresaService empresaService;
+
+    public EmpresaController(EmpresaService empresaService) {
+        this.empresaService = empresaService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Empresa> criarEmpresa(@Valid @RequestBody EmpresaDTO empresaDTO) {
+        Empresa novaEmpresa = empresaService.criarEmpresa(empresaDTO);
+        return new ResponseEntity<>(novaEmpresa, HttpStatus.CREATED);
+    }
+}
