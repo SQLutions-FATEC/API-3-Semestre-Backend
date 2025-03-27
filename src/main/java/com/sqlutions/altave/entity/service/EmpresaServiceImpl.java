f7e1658ae77541e40d0bbfa6b114b7b53e39e8c4
GloBrito <gloriafbrito@gmail.com>
2025-03-27 20:36:42 -0300

    feat: implementa serviço EmpresaServiceImpl com método criarEmpresa

---
+++ src/main/java/com/sqlutions/altave/entity/service/EmpresaServiceImpl.java
@@ -0,0 +1,32 @@
+package com.sqlutions.altave.service;
+
+import com.sqlutions.altave.dto.EmpresaDTO;
+import com.sqlutions.altave.model.Empresa;
+import com.sqlutions.altave.repository.EmpresaRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class EmpresaServiceImpl implements EmpresaService {
+
+    private final EmpresaRepository empresaRepository;
+
+    public EmpresaServiceImpl(EmpresaRepository empresaRepository) {
+        this.empresaRepository = empresaRepository;
+    }
+
+    @Override
+    @Transactional
+    public Empresa criarEmpresa(EmpresaDTO empresaDTO) {
+        if (empresaRepository.findByCnpj(empresaDTO.getCnpj()).isPresent()) {
+            throw new RuntimeException("CNPJ já cadastrado!");
+        }
+
+        Empresa empresa = new Empresa();
+        empresa.setNomeFantasia(empresaDTO.getNomeFantasia());
+        empresa.setNomeSocial(empresaDTO.getNomeSocial());
+        empresa.setCnpj(empresaDTO.getCnpj());
+
+        return empresaRepository.save(empresa);
+    }
+}
