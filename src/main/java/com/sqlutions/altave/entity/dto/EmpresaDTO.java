378f4f3a7ac7938a277100ff21dc02057c0e03cc
GloBrito <gloriafbrito@gmail.com>
2025-03-27 20:33:39 -0300

    feat: cria DTO EmpresaDTO com campos nomeFantasia, nomeSocial e cnpj

---
+++ src/main/java/com/sqlutions/altave/entity/dto/EmpresaDTO.java
@@ -0,0 +1,12 @@
+package com.sqlutions.altave.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class EmpresaDTO {
+    private String nomeFantasia;
+    private String nomeSocial;
+    private String cnpj;
+}
