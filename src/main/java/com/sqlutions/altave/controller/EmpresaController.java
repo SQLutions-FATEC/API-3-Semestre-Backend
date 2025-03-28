d118b0d0d499a105141938a37b74332cc332b942
Tiago <ttr100ttr100@gmail.com>
2025-03-27 21:10:52 -0300

    feat: habilitar configurações de setup do banco de dados no application.properties

--- src/main/java/com/sqlutions/altave/controller/EmpresaController.java
+++ src/main/java/com/sqlutions/altave/controller/EmpresaController.java
@@ -8,6 +8,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
+
 @RestController
 @RequestMapping("/empresa")
 public class EmpresaController {
