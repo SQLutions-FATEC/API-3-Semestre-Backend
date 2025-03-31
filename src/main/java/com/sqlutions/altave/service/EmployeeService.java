59d7dee589b99566a7043135b03509068a75d0da
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-03-30 23:45:49 -0300

    feat(employee): adicionado updateEmployee em EmployeeService

--- src/main/java/com/sqlutions/altave/service/EmployeeService.java
+++ src/main/java/com/sqlutions/altave/service/EmployeeService.java
@@ -1,14 +1,13 @@
 package com.sqlutions.altave.service;
 
-import com.sqlutions.altave.dto.EmployeeRequestDTO;
-import com.sqlutions.altave.entity.Employee;
+import com.sqlutions.altave.dto.EmployeeDTO;
 
 import java.util.List;
-import java.util.Optional;
 
     public interface EmployeeService {
-        Employee createEmployee(EmployeeRequestDTO EmployeeRequestDTO);
-        Optional<Employee> getEmployeeById(Long id);
-        List<Employee> getAllEmployees();
+        EmployeeDTO createEmployee(EmployeeDTO employeeDTO);
+        EmployeeDTO getEmployeeById(Long id);
+        List<EmployeeDTO> getAllEmployees();
+        EmployeeDTO updateEmployee(Long id, EmployeeDTO employeeDTO);
     }
 
