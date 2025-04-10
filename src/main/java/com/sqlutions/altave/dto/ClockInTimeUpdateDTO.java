710c9313dda629573c4886d40297edabf1a92542
Tiago <ttr100ttr100@gmail.com>
2025-04-10 09:30:55 -0300

    feat: adiciona o @JsonProperty em ClockInTimeUpdateDTO

--- src/main/java/com/sqlutions/altave/dto/ClockInTimeUpdateDTO.java
+++ src/main/java/com/sqlutions/altave/dto/ClockInTimeUpdateDTO.java
@@ -1,8 +1,10 @@
 package com.sqlutions.altave.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ClockInTimeUpdateDTO {
+    @JsonProperty("date_time")
     private String dataHora;
 }
