b2b4c5c0bb1a66445158d5decf29a5b6eaaa6fa1
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-04-23 19:42:22 -0300

    feat: adiciona métodos à interface PhotoService para manipulação de fotos

--- src/main/java/com/sqlutions/altave/service/PhotoService.java
+++ src/main/java/com/sqlutions/altave/service/PhotoService.java
@@ -1,7 +1,20 @@
 package com.sqlutions.altave.service;
 
+import com.sqlutions.altave.entity.Photo;
+import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface PhotoService {
-    void photoSave(MultipartFile foto, Long funcionarioId);
+    void photoSave(MultipartFile foto, Long employeeId);
+
+    Photo getPhotoById(Long photoId);
+    byte[] getPhotoBytes(Long photoId);
+    Resource getPhotoResource(Long photoId);
+
+    Photo getPhotoByEmployeeId(Long employeeId);
+    byte[] getPhotoBytesByEmployeeId(Long employeeId);
+    Resource getPhotoResourceByEmployeeId(Long employeeId);
+
+    void deletePhotoByEmployeeId(Long employeeId);
+
 }
