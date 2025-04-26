db0b620f3c890f9a3c3b291aaf6b7ecdc5306366
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-04-25 23:21:40 -0300

    feat(employee): adiciona método para buscar funcionário não deletado por ID

--- src/main/java/com/sqlutions/altave/repository/EmployeeRepository.java
+++ src/main/java/com/sqlutions/altave/repository/EmployeeRepository.java
@@ -4,8 +4,10 @@ import com.sqlutions.altave.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     List<Employee> findByDeletedFalse();
+    Optional<Employee> findByEmployeeIdAndDeletedFalse(Long id);
 }
 
