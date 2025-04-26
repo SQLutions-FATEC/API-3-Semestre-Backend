e8dec1d177971d307500fc63b686dbc2c6404f4c
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-04-25 23:20:50 -0300

    feat(employee): adiciona método para retornar funcionário ativo por ID

--- src/main/java/com/sqlutions/altave/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/sqlutions/altave/service/impl/EmployeeServiceImpl.java
@@ -26,8 +26,8 @@ public class EmployeeServiceImpl implements EmployeeService {
     }
     @Override
     public EmployeeDTO getEmployeeById(Long id){
-        Employee employee = employeeRepository.findById(id)
-                .orElseThrow(() -> new RuntimeException("Funcionário não encontrado"));
+        Employee employee = employeeRepository.findByEmployeeIdAndDeletedFalse(id)
+                .orElseThrow(() -> new RuntimeException("Funcionário não encontrado ou foi deletado"));
         return convertToDTO(employee);
     }
     @Override
