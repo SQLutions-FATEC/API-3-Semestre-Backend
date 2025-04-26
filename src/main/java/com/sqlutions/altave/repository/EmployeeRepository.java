dbf77c80385abc4e4f965e0e82097ca8b41c7334
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-04-25 23:07:59 -0300

    feat(employee): adiciona método findByDeletedFalse no repositório

--- src/main/java/com/sqlutions/altave/repository/EmployeeRepository.java
+++ src/main/java/com/sqlutions/altave/repository/EmployeeRepository.java
@@ -3,6 +3,9 @@ package com.sqlutions.altave.repository;
 import com.sqlutions.altave.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+    List<Employee> findByDeletedFalse();
 }
 
