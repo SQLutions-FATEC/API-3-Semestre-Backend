7d3a5bcf2c9c559b5d2545caf06e6ab44f7c6acd
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-04-27 16:22:05 -0300

    refactor: organiza estrutura de service com base atualizada

--- src/main/java/com/sqlutions/altave/service/CompanyService.java
+++ src/main/java/com/sqlutions/altave/service/CompanyService.java
@@ -1,15 +1,14 @@
 package com.sqlutions.altave.service;
 
 import com.sqlutions.altave.dto.CompanyDTO;
-import com.sqlutions.altave.dto.CompanyResponseDTO;
-import org.springframework.data.domain.Page;
 import java.util.List;
 
 public interface CompanyService {
     List<CompanyDTO> getAllCompanies();
-    CompanyResponseDTO getCompanies(int page, int size);
+    List<CompanyDTO> getAllActiveCompanies();
     CompanyDTO getCompanyById(Long id);
     CompanyDTO createCompany(CompanyDTO companyDTO);
     CompanyDTO updateCompany(Long id, CompanyDTO companyDTO);
     void deleteCompany(Long id);
+    CompanyDTO getActiveCompanyById(Long id);
 }
