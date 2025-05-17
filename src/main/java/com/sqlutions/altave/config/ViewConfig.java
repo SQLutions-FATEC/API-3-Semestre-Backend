eba06df17bc7b17ed3c05c045bdd337061e1fe21
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-05-16 21:56:58 -0300

    feat: criação automática da view analytics_summary ao iniciar a aplicação

--- src/main/java/com/sqlutions/altave/config/ViewConfig.java
+++ src/main/java/com/sqlutions/altave/config/ViewConfig.java
@@ -13,29 +13,27 @@ public class ViewConfig implements ApplicationRunner {
 
     @Override
     public void run(ApplicationArguments args) {
-        // URL do banco, usuário e senha
-        String url = "jdbc:postgresql://localhost:5432/sistema-de-ponto"; // Substitua pelo seu URL do banco
-        String user = "user";  // Substitua pelo seu usuário
-        String password = "admin";  // Substitua pela sua senha
+        String url = "jdbc:postgresql://localhost:5432/sistema-de-ponto";
+        String user = "user";
+        String password = "admin";
 
-        // SQL para DROP e CREATE da View
         String sql = """
-        DROP VIEW IF EXISTS analytics_summary;
+                        DROP VIEW IF EXISTS analytics_summary;
 
-        CREATE OR REPLACE VIEW analytics_summary AS
-        SELECT
-            e.id AS employee_id,
-            SUM(EXTRACT(EPOCH FROM (c.date_time_out - c.date_time_in)) / 3600) AS total_hours_worked,
-            e.sex
-        FROM employee e
-        JOIN clock_in c ON c.employee_id = e.id
-        GROUP BY e.id, e.sex;
-        """;
+                        CREATE OR REPLACE VIEW analytics_summary AS
+                        SELECT
+                            e.id AS employee_id,
+                            c.date_time_in,
+                            c.date_time_out,
+                            EXTRACT(EPOCH FROM (c.date_time_out - c.date_time_in)) / 3600 AS total_hours_worked,
+                            e.sex
+                        FROM employee e
+                        JOIN clock_in c ON c.employee_id = e.id;
+""";
 
         try (Connection connection = DriverManager.getConnection(url, user, password);
              Statement stmt = connection.createStatement()) {
 
-            // Executa o SQL (DROP e CREATE da VIEW)
             stmt.executeUpdate(sql);
             System.out.println("View criada com sucesso!");
 
