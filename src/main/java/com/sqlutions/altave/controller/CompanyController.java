8c0b74d1b00c1eff4f78b36f2f4be40d0f9a3650
joaopaulista <jvpaulistaa@gmail.com>
2025-05-21 10:19:08 -0300

    chore: remove import nao usados

--- src/main/java/com/sqlutions/altave/controller/CompanyController.java
+++ src/main/java/com/sqlutions/altave/controller/CompanyController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 
--- src/main/java/com/sqlutions/altave/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/sqlutions/altave/service/impl/CompanyServiceImpl.java
@@ -13,9 +13,6 @@ import com.sqlutions.altave.repository.ContractRepository;
 import com.sqlutions.altave.service.CompanyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Page;
 
 import java.util.List;
 import java.util.stream.Collectors;
