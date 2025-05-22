659676ffb59c39c2c57196a73ac3b9a42b3ab411
EnzoLFranco <fenzolemos@gmail.com>
2025-05-22 17:08:52 -0300

    fix(75): Adiciona os JsonProperty ao ContractRequestDTO
    Adiciona as property para igualar aos nomes do front-end

--- src/main/java/com/sqlutions/altave/dto/ContractRequestDTO.java
+++ src/main/java/com/sqlutions/altave/dto/ContractRequestDTO.java
@@ -1,13 +1,24 @@
 package com.sqlutions.altave.dto;
 
 import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
 public class ContractRequestDTO {
+    @JsonProperty("id")
     private Long employee_id;
+
+    @JsonProperty("company_id")
     private Long companyId;
+
+    @JsonProperty("role_id")
     private Long roleId;
+
+    @JsonProperty("datetime_start")
     private LocalDate startDate;
+
+    @JsonProperty("datetime_end")
     private LocalDate endDate;
 }
