752336f56673aab122df67574442dbab7c5f74c3
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-05-22 22:15:48 -0300

    refactor(company-service): ajusta o método getCompanies na interface

--- src/main/java/com/sqlutions/altave/service/CompanyService.java
+++ src/main/java/com/sqlutions/altave/service/CompanyService.java
@@ -1,11 +1,14 @@
 package com.sqlutions.altave.service;
 
 import com.sqlutions.altave.dto.CompanyDTO;
+import com.sqlutions.altave.dto.CompanyResponseDTO;
+
 import java.util.List;
 
 public interface CompanyService {
     List<CompanyDTO> getAllCompanies();
     List<CompanyDTO> getAllActiveCompanies();
+    CompanyResponseDTO getCompanies(int page, int size);
     CompanyDTO getCompanyById(Long id);
     CompanyDTO createCompany(CompanyDTO companyDTO);
     CompanyDTO updateCompany(Long id, CompanyDTO companyDTO);
