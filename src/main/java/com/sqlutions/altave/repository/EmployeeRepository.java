97497a34de8dead52497c4752a1deb4a7162f2b0
Lucas Eduardo de Souza Silva <lucas_souzasjc@hotmail.com>
2025-05-22 23:27:54 -0300

    refactor(employee-repository): consulta para funcionários ativos e soft delete, ajustando a lógica para a column deletedAt

--- src/main/java/com/sqlutions/altave/repository/EmployeeRepository.java
+++ src/main/java/com/sqlutions/altave/repository/EmployeeRepository.java
@@ -2,12 +2,16 @@ package com.sqlutions.altave.repository;
 
 import com.sqlutions.altave.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-    List<Employee> findByDeletedFalse();
-    Optional<Employee> findByEmployeeIdAndDeletedFalse(Long id);
+    @Query("SELECT e FROM Employee e WHERE e.deletedAt IS NULL")
+    List<Employee> findAllActive();
+
+    @Query("SELECT e FROM Employee e WHERE e.id = :id AND e.deletedAt IS NULL")
+    Optional<Employee> findByIdAndNotDeleted(Long id);
 }
 
