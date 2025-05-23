f26de71fb85f92f030b99ecd781789e6d0a021c6
EnzoLFranco <fenzolemos@gmail.com>
2025-05-23 16:07:35 -0300

    fix(75): Corrige a nomenclatura do id

--- src/main/java/com/sqlutions/altave/dto/ContractRequestDTO.java
+++ src/main/java/com/sqlutions/altave/dto/ContractRequestDTO.java
@@ -7,7 +7,7 @@ import lombok.Data;
 
 @Data
 public class ContractRequestDTO {
-    @JsonProperty("id")
+    @JsonProperty("employee_id")
     private Long employee_id;
 
     @JsonProperty("company_id")
